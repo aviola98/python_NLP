feb4cc5418fc4232b1038daa072f1f537af75d46
aviola98 <viola1@usp.br>
2021-09-15 18:42:34 -0300

    adding comments

--- ex_1.py
+++ ex_1.py
@@ -2,8 +2,13 @@ infile = open("qbdata.txt","r")
 
 #print(infile.read())
 
+#read each line of the file 
 for line in infile:
+    #split the line in values
     values = line.split()
+    #print the first and second value of each line and the last one
+    #first and secon value = name
+    #last value = rating of each athlete
     print(values[0],values[1],'teve valor',values[10])
 
 infile.close()
